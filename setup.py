f93c171c7edb28693c4a59c3ec9b2b320399c6de
LeonBytes <145725453+LeonBytes@users.noreply.github.com>
2024-07-14 15:17:12 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -75,26 +75,25 @@ class CMakeBuild(build_ext):
 
     # def _prepare_environment(self):
     #     gcc, gpp = self._find_suitable_gcc_gpp()
-
+    #
     #     gcc_path = subprocess.check_output(f"which {gcc}", shell=True).decode("utf-8").rstrip()
     #     gpp_path = subprocess.check_output(f"which {gpp}", shell=True).decode("utf-8").rstrip()
-
+    #
     #     os.environ["CC"] = gcc_path
     #     os.environ["CXX"] = gpp_path
-    
+
     def _prepare_environment(self):
-    gcc, gpp = self._find_suitable_gcc_gpp()
-    
-    if platform.system() == "Windows":
-        gcc_path = subprocess.check_output(f"where {gcc}", shell=True).decode("utf-8").rstrip()
-        gpp_path = subprocess.check_output(f"where {gpp}", shell=True).decode("utf-8").rstrip()
-    else:
-        gcc_path = subprocess.check_output(f"which {gcc}", shell=True).decode("utf-8").rstrip()
-        gpp_path = subprocess.check_output(f"which {gpp}", shell=True).decode("utf-8").rstrip()
+        gcc, gpp = self._find_suitable_gcc_gpp()
 
-    os.environ["CC"] = gcc_path
-    os.environ["CXX"] = gpp_path
+        if platform.system() == "Windows":
+            gcc_path = subprocess.check_output(f"where {gcc}", shell=True).decode("utf-8").rstrip()
+            gpp_path = subprocess.check_output(f"where {gpp}", shell=True).decode("utf-8").rstrip()
+        else:
+            gcc_path = subprocess.check_output(f"which {gcc}", shell=True).decode("utf-8").rstrip()
+            gpp_path = subprocess.check_output(f"which {gpp}", shell=True).decode("utf-8").rstrip()
 
+        os.environ["CC"] = gcc_path
+        os.environ["CXX"] = gpp_path
 
     def build_extension(self, ext):
         extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
